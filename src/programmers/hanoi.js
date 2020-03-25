0419339a361fc13a21f15d899b9c25b1b1a1172a
jess2 <jess2.jsy@gmail.com>
2020-03-25 21:32:12 +0900

    edit solution

--- src/programmers/hanoi.js
+++ src/programmers/hanoi.js
@@ -12,8 +12,7 @@ function solution (n) {
       hanoi(n-1, by, to);
     }
   }
-  hanoi(n, 1, 3); // n개의 원판을 1번 기둥에서 3번 기둥으로 옮긴
-다
+  hanoi(n, 1, 3); // n개의 원판을 1번 기둥에서 3번 기둥으로 옮긴다
   return answer;
 }
 
