099a8e97779d8893ed756c5989f2656f2d29e492
jess2 <jess2.jsy@gmail.com>
2020-05-03 17:30:53 +0900

    add solution

--- src/leetCode/easy/1389. Create Target Array in the Given Order.js
+++ src/leetCode/easy/1389. Create Target Array in the Given Order.js
@@ -4,11 +4,14 @@
  * @return {number[]}
  */
 let createTargetArray = function(nums, index) {
-  let targetArray = new Array(nums.length);
+  let targetArray = [];
 
-  for (let i = 0; i < targetArray.length; i++) {
-    targetArray[index[i]] = nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    let insertIndex = index[i];
+    targetArray.splice(insertIndex, 0, nums[i]);
   }
+
+  return targetArray;
 };
 
 console.log(createTargetArray([0,1,2,3,4], [0,1,2,2,1]));
