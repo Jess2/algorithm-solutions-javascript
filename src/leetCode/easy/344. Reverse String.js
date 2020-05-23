056e96c8c28839b5e447d4276f98a4e19fe58849
jess2 <jess2.jsy@gmail.com>
2020-05-23 12:16:26 +0900

    add solution

---
+++ src/leetCode/easy/344. Reverse String.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+let reverseString = function(s) {
+  return s.reverse();
+};
+
+console.log(reverseString(["h","e","l","l","o"]));
