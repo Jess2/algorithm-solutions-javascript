45f0f20c30244d877ab450a21a40d4bd22e50909
jessie <syjung@maxst.com>
2020-06-04 16:04:07 +0900

    add solution

---
+++ src/leetCode/easy/455. Assign Cookies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+let findContentChildren = function(g, s) {
+  g.sort((a, b) => {return b - a});
+  s.sort((a, b) => {return b - a});
+
+  let count = 0;
+
+  for (let i = 0; i < g.length; i++) {
+    for (let j = 0; j < s.length; j++) {
+      if (g[i] <= s[j]) {
+        count++;
+        s.shift();
+        break;
+      }
+    }
+  }
+  return count;
+};
+
