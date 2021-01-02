bc5ff62ff8d0d39eb840b9e522562dede820ab08
Jess2 <jess2.developer@gmail.com>
2021-01-03 00:02:08 +0900

    Add solution

--- src/leetCode/easy/387. First Unique Character in a String.js
+++ src/leetCode/easy/387. First Unique Character in a String.js
@@ -3,11 +3,32 @@
  * @return {number}
  */
 const firstUniqChar = function(s) {
-    for (let i = 0; i < s.length; i++) {
-        const str = s.slice(0, i) + s.slice(i + 1, s.length);
+    // solution 1
+    // for (let i = 0; i < s.length; i++) {
+    //     const str = s.slice(0, i) + s.slice(i + 1, s.length);
+    //
+    //     if (str.indexOf(s[i]) === -1) {
+    //         return i;
+    //     }
+    // }
+    //
+    // return -1;
 
-        if (str.indexOf(s[i]) === -1) {
-            return i;
+
+    // solution 2
+    let map = new Map();
+
+    for (let v of s) {
+        if (map.has(v)) {
+            map.set(v, map.get(v) + 1);
+        } else {
+            map.set(v, 1);
+        }
+    }
+
+    for (let [key, value] of map){
+        if (value === 1){
+            return s.indexOf(key);
         }
     }
 
