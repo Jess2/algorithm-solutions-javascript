9bb1937f1e924fc563db735978c5b380ecd9ce96
Jess2 <jess2.developer@gmail.com>
2021-01-09 23:17:14 +0900

    Add solution

---
+++ src/leetCode/medium/215. Kth Largest Element in an Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+const findKthLargest = function(nums, k) {
+    nums.sort((a, b) => {
+        return b - a;
+    });
+    return nums[k - 1];
+};
+
+console.log(findKthLargest([3,2,1,5,6,4], 2));
