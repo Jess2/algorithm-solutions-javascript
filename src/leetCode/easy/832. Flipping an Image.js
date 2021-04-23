b86186284af23e3e8698f21661b7d13616bd8691
JESS2 <jess2.developer@gmail.com>
2021-04-23 18:01:51 +0900

    Add solutions

---
+++ src/leetCode/easy/832. Flipping an Image.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+const flipAndInvertImage = function(image) {
+  for (let i = 0; i < image.length; i++) {
+    image[i].reverse();
+
+    for (let j = 0; j < image[i].length; j++) {
+      image[i][j] = image[i][j] === 0 ? 1 : 0;
+    }
+  }
+
+  return image;
+};
+
+console.log(flipAndInvertImage([[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]));
