f3014df009fa061d4e2285e85a32922fad26b601
JESS2 <jess2.developer@gmail.com>
2021-08-15 13:01:25 +0900

    Add Solution

---
+++ src/leetCode/v2/easy/283. Move Zeroes.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const moveZeroes = function(nums) {
+  for (let i = nums.length - 1; i >= 0; i--) {
+    if (nums[i] === 0) {
+      nums.splice(i, 1);
+      nums.push(0);
+    }
+  }
+};
+
+console.log(moveZeroes([0,1,0,3,12]));
+console.log(moveZeroes([0,0,1]));
